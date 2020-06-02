dafce615bed7e74f56c9230c2e10244ff4c789b5
seriogavolkov <serioga.volkov@gmail.com>
2020-06-01 22:57:02 -0600

    comeon

---
+++ build/precache-manifest.0142e02b6e0d73a95d49d1735b202fcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f865d12a823bf819ee36cf05101a09c",
+    "url": "/roboreact/index.html"
+  },
+  {
+    "revision": "c604c912d2c18aa566c9",
+    "url": "/roboreact/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1205b454e1a26855e933",
+    "url": "/roboreact/static/css/main.5fdeaf0d.chunk.css"
+  },
+  {
+    "revision": "c604c912d2c18aa566c9",
+    "url": "/roboreact/static/js/2.31e84ba2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboreact/static/js/2.31e84ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1205b454e1a26855e933",
+    "url": "/roboreact/static/js/main.022db164.chunk.js"
+  },
+  {
+    "revision": "bff3f3c158c2fc382ae3",
+    "url": "/roboreact/static/js/runtime-main.ea306fdb.js"
+  }
+]);
