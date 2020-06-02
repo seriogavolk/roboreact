c774720bf97cbb9e8597d8cc483751df858f3c71
seriogavolkov <serioga.volkov@gmail.com>
2020-06-01 22:29:32 -0600

    Final Build

---
+++ build/precache-manifest.3b1f23a71b6583e901477ec0f398d612.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a8fbe3fb7f325eaa1f8d2ae5e41a0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c604c912d2c18aa566c9",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6649279530a57e7b535f",
+    "url": "/static/css/main.5fdeaf0d.chunk.css"
+  },
+  {
+    "revision": "c604c912d2c18aa566c9",
+    "url": "/static/js/2.31e84ba2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31e84ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6649279530a57e7b535f",
+    "url": "/static/js/main.a6cc9ca9.chunk.js"
+  },
+  {
+    "revision": "459033035ba9a04d16ff",
+    "url": "/static/js/runtime-main.293d2131.js"
+  }
+]);
